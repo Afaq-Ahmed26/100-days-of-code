0ebb410d0ed1940adb7165706abd3ada7e85eb74
Afaq-Ahmed26 <afaqahmad16007@gmail.com>
2025-11-11 15:54:49 +0500

    2nd commit

--- day 1/print.py
+++ day 1/print.py
@@ -1,1 +1,11 @@
 print("hello World!")
+
+ #printing a string
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# printing multiple strings
+print("Hello" + " " + "Afaq")
+# printing with comma
+print("Hello", "Afaq")
