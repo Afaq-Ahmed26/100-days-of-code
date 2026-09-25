e60ce4cf550a44623b08f55d8c70987c747c8a8c
Afaq-Ahmed26 <afaqahmad16007@gmail.com>
2026-09-26 00:03:44 +0500

    day_4 tressure hunt game done

--- day-4/pirates_hunt.py
+++ day-4/pirates_hunt.py
@@ -1,11 +1,16 @@
-line1 = ["" , "" , ""]
-line2 = ["" , "" , ""]
-line3 = ["" , "" , ""]
+line1 = ["⬜" , "⬜" , "⬜"]
+line2 = ["⬜" , "⬜" , "⬜"]
+line3 = ["⬜" , "⬜" , "⬜"]
 map = [line1, line2, line3]
 
 print("Hiding your tressure : X marks the spot. ")
 position = input("Where do you want to hide your treasure? (e.g. 1,2): ")
 
 #actual logic code
+letter = position[0].lower()
+abc = ["a" , "b" , "c"]
+letter_index = abc.index(letter)
+number = int(position[1]) - 1
+map[number][letter_index] = "X"
 
 print(f"{line1}\n{line2}\n{line3}")
