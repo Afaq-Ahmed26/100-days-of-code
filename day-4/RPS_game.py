7980e2c413cbd306610fbc605e01338d1fbfa2f8
Afaq-Ahmed26 <afaqahmad16007@gmail.com>
2026-09-26 00:47:19 +0500

    Rock paper Scisor game

---
+++ day-4/RPS_game.py
@@ -0,0 +1,15 @@
+import random
+print("Welcome to rock paper cissors game!" )
+user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+print(f"You chose {user_choice}")
+computer_choice = random.choice(['rock', 'paper', 'scissors'])
+print(f"Computer chose {computer_choice}")
+
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif (user_choice == "rock" and computer_choice == "scissors") or \
+     (user_choice == "paper" and computer_choice == "rock") or \
+     (user_choice == "scissors" and computer_choice == "paper"):
+    print("You win!")
+else:
+    print("Computer wins!")
